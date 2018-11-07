a1287dd3a6fc720f6b3d2c7324eba183f142fb64
Lane Schwartz <dowobeha@gmail.com>
2018-11-07 15:27:31 -0600

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+let package = Package(
+  name: "CFoma",
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "CFoma",
+      targets: ["CFoma"]),
+  ],
+  dependencies: [],
+  targets: [ .target(name: "CFoma", path: ".") ]
+
+)
