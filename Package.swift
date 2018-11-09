926ca3a1fd24f20bed4cc26bec5096f34fc48803
Lane Schwartz <dowobeha@gmail.com>
2018-11-09 17:17:33 -0600

    More

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       targets: ["CFoma"]),
   ],
   dependencies: [],
-  targets: [ .systemLibrary(name: "CFoma", path: ".") ]
+  targets: [ .systemLibrary(name: "CFoma") ]
 
 )
 
